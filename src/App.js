ac451a155e37e9f6bd400a857be6ce93e0978185
Martin <martin.abilev@gmail.com>
2024-08-21 17:27:03 +0300

    add @babel/plugin-proposal-private-property-in-object

--- src/App.js
+++ src/App.js
@@ -4,13 +4,22 @@ import './App.css';
 import Home from './Home.js';
 import About from './About.js';
 
-const App = () => (
-  <div>
+// const App = () => (
+//   <div>
+//     <Routes>
+//       <Route path="/" element={<Home />}></Route>
+//       <Route path="/about" element={<About />}></Route>
+//     </Routes>
+//   </div>
+// );
+
+export default function App()
+{
+  console.log('use  server')
+  return <div>
     <Routes>
       <Route path="/" element={<Home />}></Route>
       <Route path="/about" element={<About />}></Route>
     </Routes>
   </div>
-);
-
-export default App;
+}
